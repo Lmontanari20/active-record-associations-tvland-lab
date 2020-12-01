09d02244f29f0ac90a302e5d2648dac7829cc321
Lucas Montanari <Lmonty2011@gmail.com>
2020-12-01 11:16:50 -0800

    implemented migrations and model associations/methods

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+    # A show has many characters and has many actors through characters.
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+        self.actors.map {|actor|
+            actor.full_name
+        }
+    end
 end
